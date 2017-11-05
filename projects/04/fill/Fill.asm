12885d94783b1e497d2dfc558f88ba2c7711be98
Dmitry Polyanitsa <coffeecup.winner@gmail.com>
2017-11-04 17:48:09 -0700

    Complete project 04 - Machine Language

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,35 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(:LOOP)
+  @$color
+  M = -1
+  @KBD
+  D = M
+  @:KEY
+  D ; JGT
+  @$color
+  M = 0
+(:KEY)
+  @8191
+  D = A
+  @$curr
+  M = D
+(:SCREEN_LOOP)
+  @$curr
+  D = M
+  @SCREEN
+  D = D + A
+  @$cell
+  M = D
+  @$color
+  D = M
+  @$cell
+  A = M
+  M = D
+  @$curr
+  MD = M - 1
+  @:SCREEN_LOOP
+  D ; JGE
+  @:LOOP
+  0 ; JMP
